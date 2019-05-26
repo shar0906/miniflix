7fa44feea88675d4aba9880fc9e0c6110b144440
WhereByUs <wherebyus@WhereByUss-MacBook-Pro.local>
2019-05-25 23:20:12 -0400

    react-miniflix-project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Upload from './components/Upload';
+import Display from './components/Display';
+import Callback from './components/Callback';
+import * as registerServiceWorker from './serviceWorker';
+import { Router, Route, browserHistory } from 'react-router';
+import { requireAuth } from './utils/AuthService';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={Display}/>
+        <Route path="/upload" component={Upload} onEnter={requireAuth} />
+        <Route path="/callback" component={Callback} />
+      </Router>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker.unregister();
